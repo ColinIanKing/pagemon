97ed518aaa5abf1bc8fd741ac6ad200d62cefc92
Colin Ian King <colin.king@canonical.com>
2017-01-03 11:00:57 +0000

    update copyright year
    Signed-off-by: Colin Ian King <colin.king@canonical.com>

--- pagemon.c
+++ pagemon.c
@@ -1,4 +1,6 @@
 /*
+ * Copyright (C) Colin Ian King 2015-2017
+ *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; either version 2
@@ -13,7 +15,6 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  *
- * Copyright (C) Colin Ian King 2015-2016
  * colin.i.king@gmail.com
  */
 #include <stdio.h>
--- perf.c
+++ perf.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) Colin Ian King
+ * Copyright (C) Colin Ian King 2015-2017
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
