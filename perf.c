7c77b26f0255f287e689745382f49edf0c6d3ec5
Colin Ian King <colin.king@canonical.com>
2019-01-01 16:10:43 +0000

    Update copyright year
    Signed-off-by: Colin Ian King <colin.king@canonical.com>

--- perf.c
+++ perf.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) Colin Ian King 2015-2018
+ * Copyright (C) Colin Ian King 2015-2019
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
