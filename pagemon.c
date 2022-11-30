b701a99cb54ea6bbc70c3d16b8b7b2deb9383f4b
Colin Ian King <colin.i.king@gmail.com>
2022-11-30 16:02:12 +0000

    Update copyright year
    Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

--- pagemon.c
+++ pagemon.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) Colin Ian King 2015-2021
+ * Copyright (C) Colin Ian King 2015-2022
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
